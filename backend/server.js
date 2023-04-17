efe497da480c30e9df92c8d59662798d09263990
Mourice Oronje <moronje@safaricom.co.ke>
2023-04-17 17:39:34 +0300

    commit 1

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
@@ -41,7 +42,7 @@ app.use(cors());
 app.use('/api/products', productRoute)
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
